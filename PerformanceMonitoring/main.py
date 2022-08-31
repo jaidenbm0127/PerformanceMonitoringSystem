ae2353b14f9bf993b2911059150331bd9aebd774
feshhi <jaidenbm0127@gmail.com>
2022-08-31 13:26:47 -0400

    Added some directories, gitignore, readme, and some basic starting code

---
+++ PerformanceMonitoring/main.py
@@ -0,0 +1,18 @@
+import psutil
+
+
+def main():
+    # Iterate over all running process
+    for proc in psutil.process_iter():
+        try:
+            # Get process name & pid from process object.
+            processName = proc.name()
+            processID = proc.pid
+            processCPU = proc.cpu_percent(interval=2)
+            print(processName, ' ::: ', processID, ' ::: ', processCPU)
+        except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
+            pass
+
+
+if __name__ == '__main__':
+    main()
