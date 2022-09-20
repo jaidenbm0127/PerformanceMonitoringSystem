3e0357313d4ed55ecfbe304dcf705bd5a3f2ad4a
jaidenbm0127 <jaidenbm0127@gmail.com>
2022-09-20 13:03:29 -0400

    Added gui.py

--- PerformanceMonitoring/main.py
+++ PerformanceMonitoring/main.py
@@ -1,6 +1,5 @@
 import psutil
 
-
 def main():
     # Iterate over all running process
     for proc in psutil.process_iter():
@@ -8,7 +7,7 @@ def main():
             # Get process name & pid from process object.
             processName = proc.name()
             processID = proc.pid
-            processCPU = proc.cpu_percent(interval=2)
+            processCPU = proc.cpu_percent()
             print(processName, ' ::: ', processID, ' ::: ', processCPU)
         except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
             pass
