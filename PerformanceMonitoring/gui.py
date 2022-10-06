3d6c737c1269d9199d04ff1d4e5165187073d5ae
siimr <simranjp9@gmail.com>
2022-10-05 21:43:39 -0400

    changes: overlay is changed/better

--- PerformanceMonitoring/gui.py
+++ PerformanceMonitoring/gui.py
@@ -1,18 +1,22 @@
-from tkinter import *
-import tkinter as tk
+import tkinter
+from tkinter import Text
+
+screen = tkinter.Canvas(width=300, height=300, highlightthickness=0)
+screen.master.overrideredirect(True)
+screen.master.geometry("+0+0")
+screen.master.lift()
+screen.master.wm_attributes("-topmost", True)
+screen.master.wm_attributes("-disabled", True)
+screen.create_rectangle(0, 0, 300, 300, fill="black")
+screen.master.wm_attributes("-alpha", 0.5)
+
+text = Text(screen, height= 2)
+text.pack()
+text.insert('1.0', 'This is a test ')
 
 
-root = Tk()
 
 
-root.config(bg='#add123')
-
-root.wm_attributes('-transparentcolor', '#add123')
-root.wm_attributes('-fullscreen', 'True')
-
-label = tk.Label(root, text="Hello, This is a TEST")
-label.pack()
-root.mainloop()
-
-
+screen.pack()
+screen.mainloop()
 
