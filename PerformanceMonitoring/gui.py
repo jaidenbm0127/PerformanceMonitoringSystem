298c77a688e634146705e9bdc1071e95867f87ee
Alexeeeeee <98229183+Alexeeeeee@users.noreply.github.com>
2022-10-15 21:25:45 -0400

    Made window a little darker. More clear and easy to read data.

--- PerformanceMonitoring/gui.py
+++ PerformanceMonitoring/gui.py
@@ -13,7 +13,7 @@ screen.master.lift()
 screen.master.wm_attributes("-topmost", True)
 screen.master.wm_attributes("-disabled", True)
 screen.create_rectangle(0, 0, 400, 600, fill='black')
-screen.master.wm_attributes("-alpha", 0.5)
+screen.master.wm_attributes("-alpha", 0.8)
 
 
 text = Text(screen, width=25, height=40)  # The best way to adjust the canvas screen space
@@ -21,6 +21,17 @@ text.insert('1.0', 'test')
 font_tuple = ("Times new roman", 20, "bold")  # Can adjust the font configuration
 text.configure(font=font_tuple)
 text.pack()
+
+
+def show_data(data):
+    text.insert('1.0', data)
+    text.pack()
+    return data
+
+
+show_data('Process 1\n')
+show_data('Process 2\n')
+show_data('Process 3\n')
 
 
 screen.pack()
