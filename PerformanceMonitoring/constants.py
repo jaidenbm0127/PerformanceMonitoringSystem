037d8656ce8feb3ae4b26679296946bbbffde348
feshhi <jaidenbm0127@gmail.com>
2022-11-16 14:57:38 -0500

    Added process_added and process_removed message

--- PerformanceMonitoring/constants.py
+++ PerformanceMonitoring/constants.py
@@ -4,6 +4,8 @@ CPU_PERCENTAGE_CHANGE_THRESHOLD = 50
 MEMORY_PERCENTAGE_CHANGE_THRESHOLD = 50
 GPU_PERCENTAGE_CHANGE_THRESHOLD = 100
 
+PROCESS_ADDED_MSG = "Process added: {} ::: {} ::: {} ::: {}"
+PROCESS_REMOVED_MSG = "Process removed: {} ::: {} ::: {} ::: {}"
 MEMORY_DECREASED_UTILIZATION_MSG = "Process {} with name {} decreased its MEMORY utilization by {} percent. It's current MEMORY usage is {} percent."
 MEMORY_INCREASED_UTILIZATION_MSG = "Process {} with name {} increased its MEMORY utilization by {} percent. It's current MEMORY usage is {} percent."
 CPU_DECREASED_UTILIZATION_MSG = "Process {} with name {} decreased its CPU utilization by {} percent. It's current CPU usage is {} percent."
